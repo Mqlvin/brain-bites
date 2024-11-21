59cb3457ecf241d61eafa7f1bd18b62cd1069b0a
Mqlvin <79374369+Mqlvin@users.noreply.github.com>
2024-11-21 13:11:26 +0000

    use subprocess instead of yt-dlp lib
    yt-dlp was not working so now we make raw calls to the system, you will need yt-dlp on the command line to run the program.

--- video/download.py
+++ video/download.py
@@ -1,19 +1,26 @@
-import yt_dlp
+import os
+import subprocess
+
+# Note to future selves and repo dwellers that we tried our best to use the yt-dlp library and it just doesn't work. don't waste time on it again :)
 
 # I have no idea
-def download_video(video_url, save_directory=None):
-    if save_directory is None:
-        save_directory = f"../downloads/{video_url.split("=")[1]}"
-    download_options = {
-        "write-subs": True,
-        "write-auto-subs": True,
-        "sub-lang": "en",
-        "sub-format": "webvtt",
-        "convert-subs": True,
-        "skip_download": True,
-        "outtmpl": f"{save_directory}/%(id)s.%(ext)s"
-    }
-    with yt_dlp.YoutubeDL(download_options) as ydl:
-        video_info = ydl.download([video_url])
-    return video_info
+def download_video(video_url):
+    # bit scrappy can convert this to parameter later
+    try:
+        os.makedirs("./runtime", exist_ok=True)
+    except:
+        print("[ERROR] Failed to make runtime directory")
 
+
+    args = [
+        "yt-dlp",
+        "--write-subs",
+        "--write-auto-subs",
+        "--sub-lang", "en",
+        "--sub-format", "webvtt",
+        "--skip-download",
+        "--output", "./runtime/%(id)s-transcript.%(ext)s",
+        video_url
+    ]
+
+    subprocess.run(args)
