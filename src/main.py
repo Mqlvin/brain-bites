11aee2f6e0c3b13cb96f27b2ba353d11ffc591f9
Mqlvin <79374369+Mqlvin@users.noreply.github.com>
2024-11-21 13:18:51 +0000

    add dotenv initialiser

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+from dotenv import load_dotenv
 from video.download import download_video
+
+load_dotenv()
+env_config = dotenv_values(".env")
 
 if __name__ == '__main__':
     download_video("https://www.youtube.com/watch?v=SHZAaGidUbg")
